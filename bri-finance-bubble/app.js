1d887845469491161c0823286b471813e5b83160
Joceline <joceline@potatoproductions.com>
2018-08-01 03:39:36 +0800

    add force attract, force collide, colorscale

--- bri-finance-bubble/app.js
+++ bri-finance-bubble/app.js
@@ -1,27 +1,53 @@
-var chart_width = 1000;
-var chart_height = 800;
-var padding = 50;
+let width = window.innerWidth, //chart width
+    height = window.innerHeight, // chart height
+    padding = 50; //chart padding
+
+const n = 24, //total number of nodes
+      c = 7; // number of distinct clusters
 
 
-
+//load and parse CSV
 d3.csv('bri-funds.csv').then(function(data){
-  data.forEach(function(d){ d.Amount = +d.Amount; });
-  console.log(data);
-  drawCircles(data);
+  data.forEach(function(d){ d.Amount = +d.Amount; }); //convert Amount to numbers
+  console.log(data); //log array
+  drawCircles(data); //draw circle after data loads
 })
 
 function drawCircles(data){
+
+  //set color based on Category
+  var colorScale = d3.scaleOrdinal()
+  .domain(function(d){
+    return d.Category
+  })
+  .range(["#4cdf95",
+            "#b68ef2",
+            "#6ed6aa",
+            "#b5aae9",
+            "#5cddd2",
+            "#76a4ee",
+            "#54c3e5"]);
+
+
   var simulation = d3.forceSimulation()
-  .force("x", d3.forceX(chart_width/2).strength(0.05))
-  .force("y", d3.forceY(chart_height/2).strength(0.05))
-  .force("anticollision", d3.forceCollide(function(d){
+  // .force("x", d3.forceX(width/2).strength(0.05))
+  // .force("y", d3.forceY(height/2).strength(0.05))
+  .force('center', d3.forceCenter(width/2, height/2))
+  .force('attract', d3.forceAttract()
+    .target([width/2, height/2])
+    .strength(0.01))
+  .force('cluster', d3.forceCluster()
+    .centers(function (d) { return d.Category; })
+    .strength(0.5)
+    .centerInertia(0.1))
+  .force("collide", d3.forceCollide(function(d){
     return radiusScale(d.Amount)+1
   }));
 
   var svg = d3.select('#chart')
     .append('svg')
-    .attr('width', chart_width)
-    .attr('height', chart_height)
+    .attr('width', width)
+    .attr('height', height)
     .append("g");
 
   var radiusScale = d3.scaleSqrt()
@@ -32,11 +58,13 @@ function drawCircles(data){
     .data(data)
     .enter()
     .append('circle')
-    .attr('class', 'fund')
+    .attr('class', 'financial-institution')
     .attr('r', function(d) {
       return radiusScale(d.Amount)
     })
-    .attr('fill', '#D1AB0E');
+    .attr('fill', function(d){
+      return colorScale(d.Category)
+    });
 
   simulation.nodes(data)
     .on('tick', ticked);
